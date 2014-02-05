231483996ca860d8081fd3a8b5be49dc820d47f2
agrutter87 <agrutter87@gmail.com>
2014-02-05 09:31:47 -0600

    Added motor control
    Added motor control of speed and direction to two motors

--- ROVER/ROVER.ino
+++ ROVER/ROVER.ino
@@ -3,20 +3,32 @@
 Servo panServo;
 Servo tiltServo;
 
+int motorSpeedPin[2] = {6, 5};
+int motorDirectionPin[2] = {8, 7};
 int panServoPin = 9;
 int tiltServoPin = 10;
 
+int motorSpeed[2] = {0, 0}
+boolean motorDirection[2] = {LOW, LOW};
 int panServoAngle = 0;
 int tiltServoAngle = 90;
 
 void setup()
 {
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
   panServo.attach(tiltServoPin);
   tiltServo.attach(panServoPin);
 }
 
 void loop()
 { 
+  digitalWrite(motorSpeedPin[0], motorSpeed[0]);
+  digitalWrite(motorSpeedPin[1], motorSpeed[1]);
+  digitalWrite(motorDirectionPin[0], motorDirection[0]);
+  digitalWrite(motorDirectionPin[1], motorDirection[1]);
   panServo.write(panServoAngle);
   tiltServo.write(tiltServoAngle);
 }
