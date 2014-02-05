0757e33762983dbdc04ad61d43a2dc0c77bce88d
agrutter87 <agrutter87@gmail.com>
2014-02-05 09:10:36 -0600

    Added servo control
    Added basic servo control

--- ROVER/ROVER.ino
+++ ROVER/ROVER.ino
@@ -1,9 +1,22 @@
+#include <Servo.h>
+
+Servo panServo;
+Servo tiltServo;
+
+int panServoPin = 9;
+int tiltServoPin = 10;
+
+int panServoAngle = 0;
+int tiltServoAngle = 90;
+
 void setup()
 {
-  
+  panServo.attach(tiltServoPin);
+  tiltServo.attach(panServoPin);
 }
 
 void loop()
 { 
-
+  panServo.write(panServoAngle);
+  tiltServo.write(tiltServoAngle);
 }
