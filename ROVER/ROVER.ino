7234ae6b2cfa6ce3cc117d7eaa9bc45963af43b1
agrutter87 <agrutter87@gmail.com>
2014-02-06 19:23:21 -0600

    Added infrared sensor control

--- ROVER/ROVER.ino
+++ ROVER/ROVER.ino
@@ -7,6 +7,7 @@ int motorSpeedPin[2] = {6, 5};
 int motorDirectionPin[2] = {8, 7};
 int panServoPin = 9;
 int tiltServoPin = 10;
+int irOutputPin = 12;
 
 int motorSpeed[2] = {0, 0}
 boolean motorDirection[2] = {LOW, LOW};
@@ -14,6 +15,7 @@ int panServoAngle = 0;
 int tiltServoAngle = 90;
 
 int encoderValue[2] = {0, 0};
+int irValue[4] = {0, 0, 0, 0};
 
 void setup()
 {
@@ -21,6 +23,7 @@ void setup()
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
+  pinMode(12, OUTPUT);
   panServo.attach(tiltServoPin);
   tiltServo.attach(panServoPin);
 }
@@ -33,7 +36,12 @@ void loop()
   digitalWrite(motorDirectionPin[1], motorDirection[1]);
   panServo.write(panServoAngle);
   tiltServo.write(tiltServoAngle);
+  irOutputPin = HIGH;
   
   encoderValue[0] = analogRead(A0);
   encoderValue[1] = analogRead(A1);
+  irData[0] = analogRead(A2);
+  irData[1] = analogRead(A3);
+  irData[2] = analogRead(A4);
+  irData[3] = analogRead(A5);
 }
